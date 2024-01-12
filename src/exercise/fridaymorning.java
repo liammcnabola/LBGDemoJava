1ea93f5b6da16772b8c545cbf05edb3cab2bfa97
liammcnabola <liam.mcnabola@protonmail.com>
2024-01-12 12:19:21 +0000

    first exercise

--- src/exercise/fridaymorning.java
+++ src/exercise/fridaymorning.java
@@ -1,5 +1,40 @@
 package exercise;
 
-public class fridaymorning {
+public class FridayMorning {
 
+	public static void main(String[] args) {
+		
+		char c = 'l'; System.out.println("My initial");
+		String name = "Liam"; System.out.println("My name");
+		byte bytenum = 19; System.out.println("A very small number");
+		short shrtnum = 1234; System.out.println("A slightly larger number");
+		int intnum = 1234567; System.out.println("Getting bigger");
+		long longnum = 99999999999L; System.out.println("Huge");
+		float floatnum = 11.10F; System.out.println("Decimal F");
+		double doublenum = 19.10; System.out.println("Commonly used decimal");
+		boolean b = true; System.out.println("True or false");
+		
+		String firstname = "Liam"; 
+		String surname = "McNabola";
+		System.out.println(firstname + surname); 
+		
+		char x = ' '; System.out.println("Null Character");
+		String nullname = "Liam"; System.out.println("Null name");
+		byte nullbyte = 0; System.out.println("Null number");
+		short nullshrt = 1234; System.out.println("Null number");
+		int nullint = 1234567; System.out.println("Null number");
+		long nulllong = 99999999999L; System.out.println("Null number");
+		float nullfloat = 0.0F; System.out.println("Null float");
+		double nulldouble = 0.0; System.out.println("Null double");
+		boolean nullboo = false; System.out.println("Null is false");
+		
+		char a = 'a'; 
+		char f = 'b'; 
+		System.out.println(a+f);
+		
+		char h = ' ';
+		char g = ' ';
+		System.out.println(h+g);
+	}
+	
 }
