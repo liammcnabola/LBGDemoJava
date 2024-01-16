e32b4cd87bed7edfd0c5d3f2cb4b602c5e7c9a85
liammcnabola <liam.mcnabola@protonmail.com>
2024-01-16 13:16:02 +0000

    constructors

--- src/classEx/App.java
+++ src/classEx/App.java
@@ -4,32 +4,42 @@ public class App {
 
 	public static void main(String[] args) {
 
-		Person people = new Person();
+		Person people = new Person("Liam", 30, "Banking", "Sunderland");
 
 		people.intro();
-		people.name = "Liam";
-		people.age = 30;
-		people.job = "Banking";
-		people.location = "Sunderland";
+		people.setName("Liam");
+		people.setAge(30);
+		people.setJob("Banking");
+		people.setLocation("Sunderland");
 		people.print();
 
-		Person people2 = new Person();
+		Person people2 = new Person("Robyn", 31, "Mortgages", "Sunderland");
 		people2.intro();
-		people2.name = "Robyn";
-		people2.age = 31;
-		people2.job = "Mortgages";
-		people2.location = "Sunderland";
+		people2.setName("Robyn");
+		people2.setAge(31);
+		people2.setJob("Mortgages");
+		people2.setLocation("Sunderland");
 		people2.print();
 
-		Person people3 = new Person();
+		Person people3 = new Person("Bobby", 43, "Teacher", "Scotland");
 		people3.intro();
-		people3.name = "Bob";
-		people3.age = 43;
-		people3.job = "Teacher";
-		people3.location = "Aberdeen";
+//		people3.setName("Bob");
+//		people3.setAge(43);
+//		people3.setJob("Teacher");
+//		people3.setLocation("Aberdeen");
 		people3.print();
 		
-		Person[] persons = { people, people2, people3 };
+		Person people4 = new Person("Kevin", 69, "Retired", "South Shields");
+		people4.intro();
+		people4.print();
+
+		Person people5 = new Person("Bob", "FBI", 50000);
+		people5.printSalary();
+
+		Person people6 = new Person("Steve", "Jobs", 120.65);
+		people6.printWage();
+
+		Person[] persons = { people, people2, people3, people4 };
 		for (Person person : persons) {
 			person.print();
 		
--- src/classEx/Person.java
+++ src/classEx/Person.java
@@ -2,11 +2,34 @@ package classEx;
 
 public class Person {
 
-	String name;
-	int age;
-	String job;
-	String location;
+	private String name;
+	private int age;
+	private String job;
+	private String location;
+	private String forename;
+	private String surname;
+	private int salary;
+	private double wage;
 
+	public Person(String name, int age, String job, String location) {
+		this.name = name;
+		setAge(age);
+		this.job = job;
+		this.location = location;
+	}
+
+	public Person(String name, String job, int salary) {
+		this.name = name;
+		this.job = job;
+		this.salary = salary;
+
+	}
+
+	public Person(String forename, String surname, double wage) {
+		this.forename = forename;
+		this.surname = surname;
+		setWage(wage);
+	}
 
 	void print() {
 		System.out.println("Name: " + name);
@@ -15,7 +38,76 @@ public class Person {
 		System.out.println("Location: " + location);
 	}
 
+	void printSalary() {
+		System.out.println("Name: " + name);
+		System.out.println("Job:  " + job);
+		System.out.println("Salary:  " + salary);
+	}
+
+	void printWage() {
+		System.out.println("Forename:  " + forename);
+		System.out.println("Surname:  " + surname);
+		System.out.println("Wage:  " + wage);
+	}
+
 	void intro() {
 		System.out.println("Hello! My information is as follows...");
 	}
+
+	// Setters:
+
+	public void setWage(double wage) {
+		if (wage >= 0 && wage < 1500.50) {
+			this.wage = wage;
+		} else {
+			System.out.println("Please enter a valid hourly wage");
+		}
+	}
+
+	// Getters:
+
+	public double getWage() {
+		return wage;
+	}
+
+	// Setters (continued):
+
+	public void setAge(int age) {
+		if (age >= 0 && age < 110) {
+			this.age = age;
+		} else {
+			System.out.println("Please provide a valid age");
+		}
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	};
+
+	// Getters:
+
+	public int getAge() {
+		return this.age;
+	};
+
+	public String getName() {
+		return name;
+	};
+
+	public String getJob() {
+		return job;
+	};
+
+	public String getLocation() {
+		return location;
+	};
+
 }
